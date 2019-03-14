a2b084e1572c4b88ed54093710f2265ef2c14071
savsh <savshak131@gmail.com>
2019-03-14 03:04:02 +0200

    add bey controller /bey url

---
+++ src/main/java/com/savsh/demo/controller/ByeController.java
@@ -0,0 +1,14 @@
+package com.savsh.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ByeController {
+
+    @GetMapping("/bye")
+    public String getBey() {
+        return "bey";
+    }
+
+}
